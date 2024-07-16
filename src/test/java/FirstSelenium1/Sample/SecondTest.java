7c3cd322a0c30bd0622b415572ff86d7c3df8fca
vgonaboi <venkatasomesh1999@gmail.com>
2024-07-16 20:36:25 +0530

    JenkisCode

---
+++ src/test/java/FirstSelenium1/Sample/SecondTest.java
@@ -0,0 +1,18 @@
+package FirstSelenium1.Sample;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+	@Test
+	public void run()
+	{
+		System.out.println("it is working11");
+	}
+	
+	@Test
+	public void sun()
+	{
+		System.out.println("it is sun11");
+	}
+}
